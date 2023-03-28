44804d27cb16f5671aeda18e51808578cd1a1429
dak198 <dak198@pitt.edu>
2023-03-28 18:35:31 -0400

    Initial setup

---
+++ spim/spim.py
@@ -0,0 +1,26 @@
+from typing import Literal
+
+import discord
+from redbot.core import commands
+from redbot.core.bot import Red
+from redbot.core.config import Config
+
+RequestType = Literal["discord_deleted_user", "owner", "user", "user_strict"]
+
+
+class Spim(commands.Cog):
+    """
+    Cogs for Red-DiscordBot V3 for use in Gear Getaway
+    """
+
+    def __init__(self, bot: Red) -> None:
+        self.bot = bot
+        self.config = Config.get_conf(
+            self,
+            identifier=107557231326683136,
+            force_registration=True,
+        )
+
+    async def red_delete_data_for_user(self, *, requester: RequestType, user_id: int) -> None:
+        # TODO: Replace this with the proper end user data removal handling.
+        super().red_delete_data_for_user(requester=requester, user_id=user_id)
