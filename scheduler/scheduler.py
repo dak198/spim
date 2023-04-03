b54cf540b25b0e243a2b7cbbd9a9624993fecc84
dak198 <dak198@pitt.edu>
2023-04-03 03:41:34 -0400

    send_delay expressed as float

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -22,7 +22,7 @@ class Scheduler(commands.Cog):
     @commands.command(name='message', parent=schedule, help='Schedule a message to send at specified time using `HH:MM` format')
     async def schedule_message(self, ctx, message, time_string):
         send_time = time.strptime(time_string, '%H:%M')
-        send_delay = send_time - datetime.datetime.now().timetuple()
+        send_delay = time.mktime(send_time) - time.mktime(datetime.datetime.now().timetuple())
         await ctx.send(f"sending '{message}' in 10 seconds")
         await asyncio.sleep(send_delay)
         await ctx.send(message)
