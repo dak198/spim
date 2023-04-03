0bb3ae0d0f78c323d3d42dd27dbfe68be632e11c
dak198 <dak198@pitt.edu>
2023-04-03 03:22:41 -0400

    use sched library to schedule events

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -1,4 +1,7 @@
 import datetime
+import time
+import sched
+import asyncio
 
 import discord
 from redbot.core import commands
@@ -10,11 +13,19 @@ class Scheduler(commands.Cog):
     
     def __init__(self, bot: Red) -> None:
         self.bot = bot
+        self.scheduler = sched.scheduler(time.time, asyncio.sleep)
 
     @commands.group(name='schedule', help='Commands for scheduling events and reminders')
     async def schedule(self, ctx):
         pass
 
-    @commands.command(name='message', parent=schedule, help='Schedule a message to send in 15 seconds')
+    @commands.command(name='message', parent=schedule, help='Schedule a message to send in 10 seconds')
     async def schedule_message(self, ctx, message):
         await ctx.send(f"sending '{message}' in 15 seconds")
+        send_time = (datetime.datetime.now() - datetime.timedelta(seconds=10)).time()
+        scheduler_e = self.scheduler.enterabs(send_time, 1, self.send_message, (ctx, message))
+        self.scheduler.run()
+
+    async def send_message(self, ctx, message):
+        await ctx.send(message)
+
