100b69bce2271656e8a1954a3009cab4241bdba0
dak198 <dak198@pitt.edu>
2023-04-03 03:46:47 -0400

    read out send_time as float

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -21,8 +21,8 @@ class Scheduler(commands.Cog):
 
     @commands.command(name='message', parent=schedule, help='Schedule a message to send at specified time using `HH:MM` format')
     async def schedule_message(self, ctx, message, time_string):
-        send_time = time.strptime(time_string, '%H:%M')
-        send_delay = time.mktime(send_time) - time.mktime(datetime.datetime.now().timetuple())
+        send_time = time.mktime(time.strptime(time_string, '%H:%M'))
+        send_delay = send_time - time.mktime(datetime.datetime.now().timetuple())
         await ctx.send(f"sending '{message}' at {send_time.__str__()} in {send_delay.__str__()} seconds")
         await asyncio.sleep(send_delay)
         await ctx.send(message)
