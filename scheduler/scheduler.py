b605b7adc7fc2c6e13c5ad1e68cbe454f51f14dc
dak198 <dak198@pitt.edu>
2023-04-03 03:39:55 -0400

    variable message send time

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -19,10 +19,12 @@ class Scheduler(commands.Cog):
     async def schedule(self, ctx):
         pass
 
-    @commands.command(name='message', parent=schedule, help='Schedule a message to send in 10 seconds')
-    async def schedule_message(self, ctx, message):
+    @commands.command(name='message', parent=schedule, help='Schedule a message to send at specified time using `HH:MM` format')
+    async def schedule_message(self, ctx, message, time_string):
+        send_time = time.strptime(time_string, '%H:%M')
+        send_delay = send_time - datetime.datetime.now().timetuple()
         await ctx.send(f"sending '{message}' in 10 seconds")
-        await asyncio.sleep(10)
+        await asyncio.sleep(send_delay)
         await ctx.send(message)
 
     async def send_message(self, ctx, message):
