640d6e0c3ce938eaa1fa0770fa5089fe35e75517
dak198 <dak198@pitt.edu>
2023-04-03 03:53:07 -0400

    compare datetime objects

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -21,8 +21,8 @@ class Scheduler(commands.Cog):
 
     @commands.command(name='message', parent=schedule, help='Schedule a message to send at specified time using `HH:MM` format')
     async def schedule_message(self, ctx, message, time_string):
-        send_time = time.mktime(time.strptime(time_string, '%H:%M'))
-        send_delay = time.mktime(datetime.datetime.now().timetuple()) - send_time
+        send_time = datetime.datetime.strptime(time_string, '%H:%M')
+        send_delay = (send_time - datetime.datetime.now()).total_seconds()
         await ctx.send(f"sending '{message}' at {time_string} in {send_delay} seconds")
         await asyncio.sleep(send_delay)
         await ctx.send(message)
