1ec00808c8d960fc3bce7968ea2b3c6fb3b5d63e
dak198 <dak198@pitt.edu>
2023-04-04 00:51:07 -0400

    got rid of the cabbage

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -57,12 +57,6 @@ class Scheduler(commands.Cog):
         message = await ctx.send(f"Scheduling {name} at {self.events[name]['time'].time().isoformat('auto')}")
         await message.add_reaction('<:spimPog:772261869858848779>')
         await message.add_reaction('<:spimPause:987933390110089216>')
-        for _ in range(5):
-            await ctx.send('Printing reaction list...')
-            for e in message.reactions:
-                await ctx.send(str(e.emoji))
-            await ctx.send('Done')
-            await asyncio.sleep(5)
 
         def check(reaction, user):
             return (str(reaction.emoji) == '<:spimPog:772261869858848779>' or str(reaction.emoji) == '<:spimPause:987933390110089216>') and reaction.message == message
