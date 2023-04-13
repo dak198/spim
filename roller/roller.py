187c196e9bc6b639f7cd26ecd17d74659a2c8016
dak198 <dak198@pitt.edu>
2023-04-13 18:02:54 -0400

    Python doesn't allow multiple constructure :(

--- roller/roller.py
+++ roller/roller.py
@@ -25,17 +25,22 @@ class Roller(commands.Cog):
 
     class Expression:
 
-        def __init__(self, a, b, op):
-            if not isinstance(a, type(self)):
-                raise TypeError(f"Invalid argument: {type(a)} is not instance of Expression")
-            if not isinstance(b, type(self)):
-                raise TypeError(f"Invalid argument: {type(b)} is not instance of Expression")
-            self.op = op
-            self.a = a
-            self.b = b
-
-        def __init__(self, const):
-            self.const = const
+        def __init__(self, *args):
+            if len(args) == 1:
+                self.const = args[0]
+            elif len(args) == 3:
+                a = args[0]
+                b = args[1]
+                op = args[2]
+                if not isinstance(a, type(self)):
+                    raise TypeError(f"Invalid argument: {type(a)} is not instance of Expression")
+                if not isinstance(b, type(self)):
+                    raise TypeError(f"Invalid argument: {type(b)} is not instance of Expression")
+                self.op = op
+                self.a = a
+                self.b = b
+            else:
+                raise TypeError(f"__init__() takes 1 or 3 arguments but {len(args)} were given")
 
         def evaluate(self):
             if self.const:
