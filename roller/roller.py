41d6dd992602171049c648dc419da1824db7473a
dak198 <dak198@pitt.edu>
2023-04-16 22:56:34 -0400

    Expression class supports multiple operators

--- roller/roller.py
+++ roller/roller.py
@@ -17,34 +17,66 @@ class Roller(commands.Cog):
 
     @commands.command(name="roll", help="output a random roll for a given combination of dice")
     async def roll(self, ctx, *input_string):
-        a = self.Expression(int(input_string[0]))
-        b = self.Expression(int(input_string[2]))
-        op = input_string[1]
-        expression = self.Expression(a, b, op)
+        # remove all whitespace from input string
+        expr_string = ''.join(expr_string.split())
+        expression = self.Expression(input_string)
         result = expression.evaluate()
         await ctx.send(f'{a} {op} {b} = {result}')
 
     class Expression:
 
-        def __init__(self, *args):
-            if len(args) == 1:
-                self.const = args[0]
-                self.a = self.b = self.op = None
-            elif len(args) == 3:
-                a = args[0]
-                b = args[1]
-                op = args[2]
-                if not isinstance(a, type(self)):
-                    raise TypeError(f"Invalid argument: {type(a)} is not instance of Expression")
-                if not isinstance(b, type(self)):
-                    raise TypeError(f"Invalid argument: {type(b)} is not instance of Expression")
-                self.op = op
-                self.a = a
-                self.b = b
-                self.const = None
+        def __init__(self, expr_string: str):
+            if expr_string.find('+'):
+                expr = expr_string.split('+', 1)
+                self.a = self.__init__(expr[0])
+                self.b = self.__init__(expr[1])
+                self.op = '+'
+            elif expr_string.find('-'):
+                expr = expr_string.split('-', 1)
+                self.a = self.__init__(expr[0])
+                self.b = self.__init__(expr[1])
+                self.op = '-'
+            elif expr_string.find('*'):
+                expr = expr_string.split('*', 1)
+                self.a = self.__init__(expr[0])
+                self.b = self.__init__(expr[1])
+                self.op = '*'
+            elif expr_string.find('/'):
+                expr = expr_string.split('/', 1)
+                self.a = self.__init__(expr[0])
+                self.b = self.__init__(expr[1])
+                self.op = '/'
+            elif expr_string.find('d'):
+                expr = expr_string.split('d', 1)
+                self.a = self.__init__(expr[0])
+                self.b = self.__init__(expr[1])
+                self.op = 'd'
+            elif expr_string.replace('.', '').isdigit():
+                if float(expr_string) == int(expr_string):
+                    self.const = int(expr_string)
+                else:
+                    self.const = float(expr_string)
             else:
-                raise TypeError(f"__init__() takes 1 or 3 arguments but {len(args)} were given")
-        
+                raise ValueError(f"No operator or constant found in string '{expr_string}'")
+
+            # if len(args) == 1:
+            #     self.const = args[0]
+            #     self.a = self.b = self.op = None
+            # elif len(args) == 3:
+            #     a = args[0]
+            #     b = args[1]
+            #     op = args[2]
+            #     if not isinstance(a, type(self)):
+            #         raise TypeError(f"Invalid argument: {type(a)} is not instance of Expression")
+            #     if not isinstance(b, type(self)):
+            #         raise TypeError(f"Invalid argument: {type(b)} is not instance of Expression")
+            #     self.op = op
+            #     self.a = a
+            #     self.b = b
+            #     self.const = None
+            # else:
+            #     raise TypeError(f"__init__() takes 1 or 3 arguments but {len(args)} were given")
+
         def __repr__(self):
             if self.const:
                 return repr(self.const)
