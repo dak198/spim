faad7692b3f1fac084da78ebd49bb94346b61029
dak198 <dak198@pitt.edu>
2023-04-26 13:39:40 -0400

    overhauled events to use apscheduler library

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -1,6 +1,6 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 from time import time
-from sched import scheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 import asyncio
 from uuid import uuid4
 from pytimeparse import parse
@@ -22,94 +22,84 @@ class Scheduler(commands.Cog):
 
     def __init__(self, bot: Red) -> None:
         self.bot = bot
-        self.scheduler = scheduler(time, asyncio.sleep)
+
         try:
-            self.events = load(open(FILE_PATH))
+            json_file = open(FILE_PATH)
+            self.events = load(json_file)
+            json_file.close()
         except FileNotFoundError:
             self.events = {}
+
+        self.scheduler = BackgroundScheduler()
+        for name in self.events:
+            event = self.events[name]
+            if event['remind'] and not self.scheduler.get_job(event['remind-id']):
+                self.scheduler.add_job(self.send_reminder, 'date', run_date=datetime.fromtimestamp(event['time'] - event['remind'] + event['repeat']), args=[name], id=event['remind-id'])
+            if not self.scheduler.get_job(event['id']):
+                self.scheduler.add_job(self.send_event, 'date', run_date=datetime.fromtimestamp(event['time']), args=[name], id=event['id'])
 
     def parse_args(self, *args):
         args_dict = {}
         for group in grouper(args, 2, fillvalue=None):
             flag, arg = group
             if str(flag).startswith('--'):
-                args_dict[flag] = arg
+                args_dict[flag] = arg[2:]
             else:
                 raise SyntaxError(args)
         return args_dict
     
+    async def send_reminder(self, name: str):
+        event = self.events[name]
+        # convert event time string to UTC timestamp
+        timestamp = int(round(parser.parse(timestr=event['time'], fuzzy=True).timestamp()))
+        # add @everyone to the reminder string if enabled
+        if event['notify']:
+            notify = "@everyone "
+        # send reminder string
+        reminder_string = f"{notify}**{name}** starting <t:{timestamp}:F>"
+        message = await self.bot.get_channel(event['channel-id']).send(reminder_string)
+        # save reminder message id to event data and output to json file
+        message_id = message.id
+        event['message-id'] = message_id
+        with open(FILE_PATH, 'w') as json_file:
+            dump(self.events, json_file, indent=4)
+            json_file.close()
+        # add reactions to reminder message for users to indicate 'attending' or 'absent'
+        await message.add_reaction('<:spimPog:772261869858848779>')
+        await message.add_reaction('<:spon:922922345134424116>')
+        if event['repeat']:
+            self.scheduler.add_job(self.send_reminder, 'date', run_date=datetime.fromtimestamp(event['time'] - event['remind'] + event['repeat']), args=[name], id=event['remind-id'])
+
+    async def send_event(self, name: str):
+        event = self.events[name]
+        if event['notify']:
+            notify = "@everyone "
+        await self.bot.get_channel(event['channel-id']).send(f"{notify}**{name}** starting now")
+        if self.events['repeat']:
+            event['time'] += event['repeat']
+            self.scheduler.add_job(self.send_event, 'date', run_date=datetime.fromtimestamp(event['time']), args=[name], id=event['id'])
+
+
     async def add_event(self, ctx, name: str, event: dict):
         # add event to event list
         self.events[name] = event
         # update event list in external file
         with open(FILE_PATH, 'w') as json_file:
             dump(self.events, json_file, indent=4)
-            json_file.close()
+
+        if event['remind']:
+            self.scheduler.add_job(self.send_reminder, 'date', run_date=datetime.fromtimestamp(event['time'] - event['remind']), args=[name], id=event['id'])
+        self.scheduler.add_job(self.send_event, 'date', run_date=datetime.fromtimestamp(event['time']), args=[name], id=event['id'])
 
         # print event info to the chat
-        message_string = f"Scheduling `{name}` at `{parser.parse(timestr=self.events[name]['time'], fuzzy=True).time().isoformat('auto')}`."
-        if self.events[name]['repeat']:
-            message_string += f" Repeating every `{self.events[name]['repeat']}` seconds."
-        if self.events[name]['remind']:
-            message_string += f" Sending reminder `{self.events[name]['remind']}` seconds before event."
-        if self.events[name]['notify']:
+        message_string = f"Scheduling `{name}` at `{parser.parse(timestr=event['time'], fuzzy=True).time().isoformat('auto')}`."
+        if event['repeat']:
+            message_string += f" Repeating every `{event['repeat']}` seconds."
+        if event['remind']:
+            message_string += f" Sending reminder `{event['remind']}` seconds before event."
+        if event['notify']:
             message_string += ' Notifying with `@everyone`'
         await ctx.send(message_string)
-
-        # loop in case of repeated event
-        while name in self.events:
-            # calculate wait times
-            event_delay = (parser.parse(timestr=self.events[name]['time'], fuzzy=True) - datetime.now()).total_seconds()
-            if self.events[name]['remind']:
-                remind_delay = event_delay - self.events[name]['remind']
-            else:
-                remind_delay = 0
-            # skip waiting for the reminder if it is before the event time
-            if remind_delay > 0:
-                # sleep thread until reminder time
-                await asyncio.sleep(remind_delay)
-                # update events list from file in case of changes during sleep
-                self.events = load(open(FILE_PATH))
-                # make sure event is still exists when done waiting
-                # validate id as well as name in case the event was cancelled and replaced with a new event with the same name
-                if name in self.events and self.events[name]['id'] == event['id']:
-                        # send reminder based on provided arguments
-                        reminder_string = f"**{name}** at {parser.parse(timestr=self.events[name]['time'], fuzzy=True)}"
-                        if self.events[name]['notify']:
-                            reminder_string = '@everyone ' + reminder_string
-                        # only generate new reminder message if there is not already one linked to this event
-                        if not self.events[name]['message-id']:
-                            message = await ctx.send(reminder_string)
-                            # save reminder message id to event data and output to json file
-                            message_id = message.id
-                            self.events[name]['message-id'] = message_id
-                            with open(FILE_PATH, 'w') as json_file:
-                                dump(self.events, json_file, indent=4)
-                                json_file.close()
-                            # add reactions to reminder message for users to indicate 'attending' or 'absent'
-                            await message.add_reaction('<:spimPog:772261869858848779>')
-                            await message.add_reaction('<:spon:922922345134424116>')
-
-            # sleep thread until event time
-            await asyncio.sleep(event_delay)
-            # make sure event still exists when done waiting
-            if name in self.events:
-                # announce start of event
-                event_string = f"**{name}** starting now"
-                if self.events[name]['notify']:
-                    event_string = '@everyone ' + event_string
-                await ctx.send(event_string)
-                # unlink the old reminder message
-                self.events[name]['message-id'] = None
-                # if the event repeats, set the time to the next repeat interval
-                if self.events[name]['repeat']:
-                    self.events[name]['time'] = parser.parse(timestr=self.events[name]['time'], fuzzy=True) + timedelta(seconds=self.events[name]['repeat'])
-                # if the event does not repeat, remove it from the events list and update the json file
-                else:
-                    self.events.pop(name, None)
-                    with open(FILE_PATH, 'w') as json_file:
-                        dump(self.events, json_file, indent=4)
-                        json_file.close()
 
     @commands.group(name='event', help='Commands for managing events and reminders')
     async def event(self, ctx):
@@ -129,8 +119,8 @@ class Scheduler(commands.Cog):
         # parse the provided arguments into a dict
         options = self.parse_args(*args)
         # check for if an event name was provided, return if not
-        if '--name' in options:
-            name = options['--name']
+        if 'name' in options:
+            name = options['name']
             # return if event name already exists
             if name in self.events:
                 await ctx.send('Event with that name already exists')
@@ -142,6 +132,8 @@ class Scheduler(commands.Cog):
         # create an empty event with default values and a unique id
         event = {
             'id': uuid4().hex,
+            'remind-id': uuid4().hex,
+            'channel-id': ctx.channel.id,
             'message-id': None,
             'time': 'Saturday at 3:00pm',
             'repeat': None,
@@ -152,15 +144,15 @@ class Scheduler(commands.Cog):
         }
 
         # configure the empty event with the provided arguments
-        if '--time' in options:
-            event['time'] = options['--time']
+        if 'time' in options:
+            event['time'] = int(round(parser.parse(timestr=options['time'], fuzzy=True).timestamp()))
         else:
             await ctx.send('No time specified, defaulting to Saturday at 3:00pm')
-        if '--repeat' in options:
-            event['repeat'] = parse(options['--repeat'], granularity='minutes')
-        if '--remind' in options:
-            event['remind'] = parse(options['--remind'], granularity='minutes')
-        if '--notify' in options and options['--notify'].lower() == 'true':
+        if 'repeat' in options:
+            event['repeat'] = parse(options['repeat'], granularity='minutes')
+        if 'remind' in options:
+            event['remind'] = parse(options['remind'], granularity='minutes')
+        if 'notify' in options and options['notify'].lower() == 'true':
             event['notify'] = True
         # process the newly added event
         await self.add_event(ctx, name, event)
@@ -169,32 +161,34 @@ class Scheduler(commands.Cog):
     async def event_cancel(self, ctx, name):
         # attempt to remove event with given name from event list
         event = self.events.pop(name, None)
-        # if event was successfully removed, update the json file
         if event:
-            await ctx.send(f"Removed {name}")
+            # update the json file
             with open(FILE_PATH, 'w') as json_file:
                 dump(self.events, json_file, indent=4)
-                json_file.close()
+            # remove associated jobs from scheduler
+            if self.scheduler.get_job(event['id']):
+                self.scheduler.remove_job(event['id'])
+            if self.scheduler.get_job(event['remind-id']):
+                self.scheduler.remove_job(event['remind-id'])
+            await ctx.send(f"Removed {name}")
         else:
             await ctx.send(f'{name} not found in events list')
 
     @commands.command(name='edit', parent=event, help='Edit an existing event')
     async def event_edit(self, ctx, *args):
-        # parse the provided arguments into a dict
+        # parse provided arguments into a dict
         options = self.parse_args(*args)
         # check for if an event name was provided
-        name = options.pop('--name', None)
+        name = options.pop('name', None)
         if name:
-            # pop event from event list and ensure it exists
-            event = self.events.pop(name, None)
-            if event:
-                # update the event with the parameters that were specified in options (excluding --name)
+            if name in self.events:
+                # update event with parameters specified in options (excluding name)
                 for flag in options:
-                    event[flag] = options[flag]
-                # generate a new id for the event so that the previous version will be ignored
-                event['id'] = uuid4().hex
-                # add the event back to the list
-                await self.add_event(ctx, name, event)
+                    self.events[name][flag] = options[flag]
+                # reschedule associated jobs in scheduler
+                self.scheduler.reschedule_job(self.events[name]['id'], trigger='date', run_date=self.events[name]['time'])
+                if self.events[name]['remind']:
+                    self.scheduler.reschedule_job(self.events[name]['remind-id'], trigger='date', run_date=self.events[name]['remind'])
             else:
                 await ctx.send('Event with that name does not exist')
         else:
@@ -220,15 +214,6 @@ class Scheduler(commands.Cog):
             text = '```No events scheduled```'
         await ctx.send(text)
 
-    @commands.command(name='repeat-message')
-    async def set_repeat_message(self, ctx, state):
-        if state == 'start':
-            self.repeat_message.start()
-            await ctx.send('Started repeat message')
-        elif state == 'stop':
-            self.repeat_message.stop()
-            await ctx.send('Stopped repeat message')
-
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
         user = payload.member
@@ -253,8 +238,7 @@ class Scheduler(commands.Cog):
                             if not user.id in event['absent']:
                                 event['absent'][user.id] = user.display_name
                         with open(FILE_PATH, 'w') as json_file:
-                                    dump(self.events, json_file, indent=4)
-                                    json_file.close()
+                            dump(self.events, json_file, indent=4)
 
     @commands.Cog.listener()
     async def on_raw_reaction_remove(self, payload):
@@ -271,10 +255,4 @@ class Scheduler(commands.Cog):
                         elif emoji.name == 'spon':
                             event['absent'].pop(user_id, None)
                         with open(FILE_PATH, 'w') as json_file:
-                                    dump(self.events, json_file, indent=4)
-                                    json_file.close()
-
-    @tasks.loop(seconds=15.0)
-    async def repeat_message(self):
-        channel = self.bot.get_channel(661373412400431104)
-        await channel.send("Test")
+                            dump(self.events, json_file, indent=4)
