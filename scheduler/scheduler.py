b56e096c4fce295a93e8f820f860ed9539529828
dak198 <dak198@pitt.edu>
2023-05-04 23:02:11 -0400

    missed a walrus

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -217,7 +217,7 @@ class Scheduler(commands.Cog):
                 elif event['repeat']:
                     self.scheduler.reschedule_job(event['remind-id'], trigger='date', run_date=datetime.fromtimestamp(remind_time + event['repeat']))
             else:
-                if event['remind'] and (remind_time := event['time'] - event['remind']) > datetime.now().timestamp():
+                if remind_time > datetime.now().timestamp():
                     self.scheduler.add_job(self.send_reminder, 'date', run_date=datetime.fromtimestamp(remind_time), args=[name], id=event['remind-id'])
                 elif event['repeat']:
                     self.scheduler.add_job(event['remind-id'], trigger='date', run_date=datetime.fromtimestamp(remind_time + event['repeat']))
