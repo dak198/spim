fa6926b370548e778a4683a00529bd6b15d18bf4
dak198 <dominickarras@gmail.com>
2024-08-11 22:27:21 -0400

    removed poll from __init__.py

--- spim/__init__.py
+++ spim/__init__.py
@@ -2,7 +2,7 @@ import json
 import discord
 from pathlib import Path
 from redbot.core.bot import Red
-from .spim import Spim, spimify, poll
+from .spim import Spim, spimify
 
 with open(Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
@@ -11,8 +11,6 @@ with open(Path(__file__).parent / "info.json") as fp:
 async def setup(bot: Red) -> None:
     await bot.add_cog(Spim(bot))
     bot.tree.add_command(spimify)
-    bot.tree.add_command(poll)
 
 async def teardown(bot: Red):
     bot.tree.remove_command("Spimify", type=discord.AppCommandType.user)
-    bot.tree.remove_command("Poll", type=discord.AppCommandType.user)
